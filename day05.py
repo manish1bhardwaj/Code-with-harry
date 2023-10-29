49bcf545e68b4d17b740113857a2783252fc8c18
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2023-10-29 22:26:32 +0530

    codes practice

---
+++ day05.py
@@ -0,0 +1,39 @@
+##print('hey i am manish\nand i am from rewari haryana')
+##print('hey i am manish\nand i am from rewari haryana')
+##print('hey i am manish\nand i am from rewari haryana')
+##print("\'this\'\"python programming\"",2,3,4,4,end="\n") 
+##print("manish")
+##a=10
+##b=29
+##c="manish"
+##d=None
+##e=True
+##print(a)
+##print(b)
+##print(c)
+ ##print(a+b+e)
+##print("this is a type of a",type(a))
+##print("this is a type of a",type(b))
+##print("this is a type of a",type(c))
+##print("this is a type of a",type(d))
+##print("this is a type of a",type(e))
+
+a=input('enter the first no  :')
+b=input('enter the second no :')
+##print(a+b)
+##print(a-b)
+##print(a*b)
+##print(a/b)
+##print(a//b)
+##print(a%b)
+##print(a**b)
+
+
+##print(int(a)+int(b))
+##print(int(a)-int(b))
+##print(int(a)*int(b))
+##print(int(a)/int(b))
+##print(int(a)//int(b))
+##print(int(a)%int(b))
+##print(int(a)**int(b))
+##print(int(a)+int(b))
